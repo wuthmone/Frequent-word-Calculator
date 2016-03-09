6e96e3033da671c8efa2be47b632281a398c3edf
wuthmone <whlaing10@gmail.com>
2016-03-09 19:56:10 +0800

    completed

--- module2_assignment.rb
+++ module2_assignment.rb
@@ -2,86 +2,81 @@
 
 #Implement a class called LineAnalyzer.
 class LineAnalyzer
+    attr_accessor :highest_wf_count,:highest_wf_words,:content,:line_number
   
 
-    attr_accessor :highest_wf_count, :highest_wf_words, :content, :line_number
-    @highest_wf_words = []
-
-    def initialize (text_line,line_no)
-      @content = text_line 
-      @line_number = line_no
+    def initialize (text,line_number)
+      @content = text
+      @line_number = line_number
+        @highest_wf_words = []
       calculate_word_frequency()
     end
-    def self. calculate_word_frequency ()
-        @content = content.chomp.split
+    class << self 
+    	
+    end
+    def calculate_word_frequency ()
+        tocount = content.split
         count = Hash.new(0)
-        content.each {|content| count[content.downcase] += 1}
+        tocount.each {|element| count[element.downcase] += 1}
         count = count.sort_by {|x,y| y}
         count = count.reverse!
-        highest = count[0].to_a
-        @highest_wf_count = highest[1]
-        @highest_wf_words << highest[0]
-        
+        @highest_wf_count = count.first[1]
+		count.each do |key,value|
+		 if value == highest_wf_count
+		 	@highest_wf_words << key
+		 end
+		end
+
     end
 end
 
 #  Implement a class called Solution. 
 class Solution
   attr_accessor :analyzer,:highest_count_across_lines,:highest_count_words_across_lines
-    @highest_count_words_across_lines = []
+    
 
     
     def initialize()
-      @analyzer = []
+      @analyzers = []
+      @highest_count_across_lines = 0
+      @highest_count_words_across_lines = []
     end
 
     def analyze_file()
-      
-      line_num = 1
-      
-      begin
-
+      line_number = 1
+    
         File.foreach('test.txt') do |line|  
-          @analyzer << LineAnalyzer.new(line,line_num)
-          line_num +=  1
+          @analyzers << LineAnalyzer.new(line,line_number)
+          line_number +=  1
         end
-
-        rescue Exception => e
-          puts e.message
-          puts "File do not exit"
-        end
-
-      end
-     
     end
 
-    def calculate_line_with_higest_frequency()
-      @highest_count_across_lines = 0
-      unless analyzer.empty?
-        analyzer.each do |element| 
-         if element.highest_wf_count > highest_count_across_lines
+    def calculate_line_with_highest_frequency()
+      unless @analyzers.empty?
+        @analyzers.each do |element| 
+         if element.highest_wf_count > highest_count_across_lines 
           @highest_count_across_lines = element.highest_wf_count
          end
         end 
       end
-
-      unless analyzer.empty?
-        analyzer.each do |element|
-          if @highest_count_words_across_lines == element.highest_wf_count
-            @highest_count_words_across_lines << element
+      unless @analyzers.empty?
+        @analyzers.each do |element|
+          if @highest_count_across_lines == element.highest_wf_count
+            @highest_count_words_across_lines << element 
           end
         end
       end
     end
 
-    def print_hightest_word_frequency_across_lines()
+    def print_highest_word_frequency_across_lines()
       puts "The following words have the highest word frequency per line:"
-      unless print_hightest_word_frequency_across_lines.empty?
-        highest_count_words_across_lines.each do |element|
-          puts "#{[element.highest_wf_words]}(appears in line #{element.line_number})"
-          
+      unless highest_count_words_across_lines.empty?
+        @highest_count_words_across_lines.each do |element|
+        	word= element.highest_wf_words
+        	num = element.line_number
+        	
+         puts "#{word} appears in line #{element.line_number})"
         end
-      
+      end
     end
 end
-
